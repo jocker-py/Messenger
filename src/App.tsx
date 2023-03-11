e8a9efab58cded6855923ede1553bcede25e6ed3
jocker-py <buladzislau@gmail.com>
2023-03-11 13:22:53 +0300

    feat: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Profile from './components/Profile';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hello, samurai! Let's go!
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
