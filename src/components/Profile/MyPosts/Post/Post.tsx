af3173a52a2bc7eb79ed82ec3424f25cfeed8881
jocker-py <buladzislau@gmail.com>
2023-03-13 19:28:56 +0300

    feat: add message and likes from props

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -3,15 +3,15 @@ import styles from "./Post.module.css";
 
 export type PostType = {likes: number, message: string};
 
-const Post = () => {
+const Post = (props: PostType) => {
   return (
     <div className={styles.item}>
       <img className={styles.img}
          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0eQbUCAZ2d69KWQrjdiu7lepxp3Yswe18Ozhk4B0jDQ&s"
          alt="avatar"/>
-    post
+      {props.message}
       <div>
-        <span>like</span>
+        <span>{'likes: ' + props.likes}</span>
       </div>
   </div>)
 }
