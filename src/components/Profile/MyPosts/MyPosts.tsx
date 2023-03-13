23b4c1e8d43df11154df5f3789f843013a4c24ac
jocker-py <buladzislau@gmail.com>
2023-03-13 17:56:33 +0300

    refactor: replace part of Profile component to MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from "../Profile.module.css";
 
 const MyPosts = () => {
-  return (<div></div>);
+  return (
+    <div>My posts
+    <h2>New posts</h2>
+    <div>
+      <div className={styles.item}>post1</div>
+      <div className={styles.item}>post2</div>
+    </div>
+  </div>
+  );
 }
 
 export default MyPosts;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import styles from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
     <div className={styles.content}>
       <img className={styles.content__img} src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="mountains"/>
       <div>ava + description</div>
-      <div>My posts
-        <h2>New posts</h2>
-        <div>
-          <div className={styles.item}>post1</div>
-          <div className={styles.item}>post2</div>
-        </div>
-      </div>
+      <MyPosts />
     </div>
   );
 }
