644d1b66ebeb0b7fbb0ddf63637ae5c2d6e78010
jocker-py <buladzislau@gmail.com>
2023-03-13 16:41:26 +0300

    feat: add className item and style for it

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,10 +7,10 @@ const Profile = () => {
       <img className={styles.content__img} src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="mountains"/>
       <div>ava + description</div>
       <div>My posts
-        <div>New posts</div>
+        <h2>New posts</h2>
         <div>
-          <div>post1</div>
-          <div>post2</div>
+          <div className={styles.item}>post1</div>
+          <div className={styles.item}>post2</div>
         </div>
       </div>
     </div>
