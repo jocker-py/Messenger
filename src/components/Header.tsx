8a80531e3a2185c350e5e53c65597c4bce007c39
jocker-py <buladzislau@gmail.com>
2023-03-13 16:34:32 +0300

    refactor: change className using module.css file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className='header'>
-      <img src="https://i.pinimg.com/originals/b3/3a/42/b33a42368fdaff13589f87739c2e7a44.jpg" alt="logo"/>
+    <header className={styles.header}>
+      <img className={styles.header__img} src="https://i.pinimg.com/originals/b3/3a/42/b33a42368fdaff13589f87739c2e7a44.jpg" alt="logo"/>
     </header>
   );
 }
