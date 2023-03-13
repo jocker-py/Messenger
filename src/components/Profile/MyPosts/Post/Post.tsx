322b0881e0c5bafb837b03c18e91d74527639af9
jocker-py <buladzislau@gmail.com>
2023-03-13 18:04:27 +0300

    feat: add img

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import styles from "./Post.module.css";
 
 const Post = () => {
-  return (<div className={styles.item}>post1</div>)
+  return (
+    <div className={styles.item}>
+      <img className={styles.img}
+         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0eQbUCAZ2d69KWQrjdiu7lepxp3Yswe18Ozhk4B0jDQ&s"
+         alt="avatar"/>
+    post
+  </div>)
 }
 
 export default Post;
