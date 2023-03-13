9400dbfd1635a634de27eb4c93237d8b9de37e33
jocker-py <buladzislau@gmail.com>
2023-03-13 16:28:37 +0300

    refactor: change alt

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Profile = () => {
   return (
     <div className='content'>
-      <img src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="image mountains"/>
+      <img src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="mountains"/>
       <div>ava + description</div>
       <div>My posts
         <div>New posts</div>
