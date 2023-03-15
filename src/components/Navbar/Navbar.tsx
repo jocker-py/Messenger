b00c4ae27dec84523643261a6de06274c9af09e0
jocker-py <buladzislau@gmail.com>
2023-03-15 18:23:21 +0300

    feat: add id and href for NavbarLinkPropsType

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,52 @@
 import React from "react";
 import styles from './Navbar.module.css';
 
+type NavbarLinkPropsType = {
+  title: string;
+  href: string;
+  id: string;
+};
+
 const Navbar = () => {
-  const links = ['Profile', 'Messages', 'News', 'Music', 'Settings'];
+  const links: NavbarLinkPropsType[] = [{
+    title: 'Profile',
+    href: '/profile',
+    id: 'link_1',
+  }, {
+    title: 'Messages',
+    href: '/dialogs',
+    id: 'link_2',
+  }, {
+    title: 'News',
+    href: '/news',
+    id: 'link_3',
+  }, {
+    title: 'Music',
+    href: '/music',
+    id: 'link_4',
+  }, {
+    title: 'Settings',
+    href: '/settings',
+    id: 'link_5',
+  }];
   return (
     <nav className={styles.nav}>
-      {links.map((title, idx) => <Link key={idx} title={title} />)}
+      {links.map(({title, href, id}) => <Link key={id}
+                                              title={title}
+                                              href={href}
+                                              id={id} />)}
     </nav>
   );
 }
 
-type NavbarLinkPropsType = {
-  title: string;
-};
-
 function Link(props:NavbarLinkPropsType){
-  return <div><a href="#" className={styles.link}>{props.title}</a></div>
+  return (<div>
+    <a id={props.id}
+       href={props.href}
+       className={styles.link}>
+      {props.title}
+    </a>
+  </div>)
 }
 
 export default Navbar;
