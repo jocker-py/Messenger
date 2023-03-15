1ffca95145e37aeb07c430f31af2e6e0cec4012a
jocker-py <buladzislau@gmail.com>
2023-03-15 18:12:52 +0300

    feat: add BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        <Profile />
-        {/*<Dialogs />*/}
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Profile />
+          <Dialogs />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
+
   );
 }
 
