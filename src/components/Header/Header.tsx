d9a6559725cae3a0c2f37fc8c1a087e0150dc3fa
jocker-py <buladzislau@gmail.com>
2023-03-16 15:07:07 +0300

    feat: add css variables

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img className={styles.header__img} src="https://i.pinimg.com/originals/b3/3a/42/b33a42368fdaff13589f87739c2e7a44.jpg" alt="logo"/>
+      <img className={styles.img} src="https://i.pinimg.com/originals/b3/3a/42/b33a42368fdaff13589f87739c2e7a44.jpg" alt="logo"/>
     </header>
   );
 }
