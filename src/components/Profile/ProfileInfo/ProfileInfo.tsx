691640836aec9dc760e58f74099306a5cc17ef8b
jocker-py <buladzislau@gmail.com>
2023-03-16 14:39:16 +0300

    feat: add container for img

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,9 +3,11 @@ import styles from "./ProfileInfo.module.css";
 
 const ProfileInfo = () => {
   return (<div>
-      <img className={styles.img}
-           src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600"
-           alt="mountains"/>
+      <div>
+        <img className={styles.img}
+             src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600"
+             alt="mountains"/>
+      </div>
       <div className={styles.description}>ava + description</div>
     </div>)
 }
