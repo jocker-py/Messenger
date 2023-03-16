df4a9b275f42615b4bf848644a87aec9b73f64a4
jocker-py <buladzislau@gmail.com>
2023-03-16 12:55:02 +0300

    feat: add dialogs list

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import styles from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (<div>
-    Dialogs
+    <ul className={styles.dialogs__list}>
+      <li className={styles.dialogs__item}>Dimych</li>
+      <li className={styles.dialogs__item}>Andrey</li>
+      <li className={styles.dialogs__item}>Sveta</li>
+      <li className={styles.dialogs__item}>Sasha</li>
+      <li className={styles.dialogs__item}>Viktor</li>
+      <li className={styles.dialogs__item}>Valera</li>
+    </ul>
   </div>)
 }
 
