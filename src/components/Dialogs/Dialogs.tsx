e042b327a058f1f4c6a0767c3a53220dd1400b6b
jocker-py <buladzislau@gmail.com>
2023-03-16 14:04:32 +0300

    feat: add DialogsItem component and type for it

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Dialogs.module.css';
+
+type DialogsItemType = {
+  id: number;
+  name: string;
+}
+
+const DialogsItem = (props: DialogsItemType) => {
+  let path = '/dialogs/' + props.id;
+  return (<NavLink to={path}
+                   className={styles.dialogs__item}
+                   activeClassName={styles.dialogs__item_active}>{props.name}</NavLink>)
+}
 
 const Dialogs = () => {
   return (<div className={styles.dialogs}>
     <div className={styles.dialogs__list}>
-      <div className={styles.dialogs__item_active}>Dimych</div>
-      <div className={styles.dialogs__item}>Andrey</div>
-      <div className={styles.dialogs__item}>Sveta</div>
-      <div className={styles.dialogs__item}>Sasha</div>
-      <div className={styles.dialogs__item}>Viktor</div>
-      <div className={styles.dialogs__item}>Valera</div>
+      <DialogsItem name={"Dimych"} id={1}/>
+      <DialogsItem name={"Andrey"} id={2}/>
+      <DialogsItem name={"Sveta"} id={3}/>
+      <DialogsItem name={"Sasha"} id={4}/>
+      <DialogsItem name={"Viktor"} id={5}/>
+      <DialogsItem name={"Valera"} id={6}/>
     </div>
     <div className={styles.messages__list}>
       <div className={styles.messages__list}>Hi</div>
