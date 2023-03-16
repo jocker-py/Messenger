70893e6563268cf515f3483bcb6ffbc9fbc377f6
jocker-py <buladzislau@gmail.com>
2023-03-16 22:59:03 +0300

    refactor: use state instead of links

---
+++ src/components/Sidebar/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from './Navbar.module.css';
+
+type NavbarType = {state: NavbarLinkType[]};
+const Navbar = (props: NavbarType) => {
+  return (
+      <nav className={styles.nav}>
+        {props.state.map(({title, href, id}) =>
+          <Link key={id} title={title} href={href} id={id} />)}
+      </nav>
+  );
+}
+
+export type NavbarLinkType = { title: string; href: string; id: number; };
+function Link(props:NavbarLinkType){
+  return (<div>
+    <NavLink to={props.href} className={styles.link} activeClassName={styles.active}>
+      {props.title}
+    </NavLink>
+  </div>)
+}
+
+export default Navbar;
