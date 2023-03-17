c3c7feb53341998115fc85537d4da5b00339aba7
jocker-py <buladzislau@gmail.com>
2023-03-17 14:06:22 +0300

    refactor: create dialogsElements and messagesElements

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,15 +6,13 @@ import {DialogsPageType} from "../../redux/state";
 
 type DialogsType = {state:DialogsPageType};
 const Dialogs = (props: DialogsType) => {
+  const dialogsElements = props.state.dialogs.map(
+    ({id, name}) => <Dialog key={id} name={name} id={id} />);
+  const messagesElements = props.state.messages.map(
+    ({id, message}) => <Message key={id} message={message} id={id} />)
   return (<div className={styles.dialogs}>
-    <div className={styles.dialogList}>
-      {props.state.dialogs.map(
-        ({id, name}) => <Dialog key={id} name={name} id={id} />)}
-    </div>
-    <div className={styles.messagesList}>
-      {props.state.messages.map(
-        ({id, message}) => <Message key={id} message={message} id={id} />)}
-    </div>
+    <div className={styles.dialogList}>{dialogsElements}</div>
+    <div className={styles.messagesList}>{messagesElements}</div>
   </div>)
 }
 
