16a25401106f9712d6b701d692459a30a5c08b54
jocker-py <buladzislau@gmail.com>
2023-03-17 14:23:28 +0300

    feat: add actions with textarea in Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {LegacyRef} from 'react';
 import styles from './Dialogs.module.css';
 import Message from "./Message/Message";
 import Dialog from "./Dialog/Dialog";
@@ -10,9 +10,24 @@ const Dialogs = (props: DialogsType) => {
     ({id, name}) => <Dialog key={id} name={name} id={id} />);
   const messagesElements = props.state.messages.map(
     ({id, message}) => <Message key={id} message={message} id={id} />)
+  const newMessageElement: LegacyRef<HTMLTextAreaElement> = React.createRef();
+  const showMessage = () => {
+    alert(newMessageElement.current!.value);
+    newMessageElement.current!.value = '';
+  }
   return (<div className={styles.dialogs}>
     <div className={styles.dialogList}>{dialogsElements}</div>
-    <div className={styles.messagesList}>{messagesElements}</div>
+    <div>
+      <div className={styles.messagesList}>{messagesElements}</div>
+      <div>
+        <div>
+          <textarea ref={newMessageElement}></textarea>
+        </div>
+        <div>
+          <button onClick={showMessage}>Add message</button>
+        </div>
+      </div>
+    </div>
   </div>)
 }
 
