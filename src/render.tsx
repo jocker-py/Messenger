a6885f2c34d988c999b3227afac3caa2ee91d5ad
jocker-py <buladzislau@gmail.com>
2023-03-17 15:31:52 +0300

    feat: add type for renderEntireThree

--- src/render.tsx
+++ src/render.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {addPost, StateType} from './redux/state';
 
-const renderEntireThree = (state:StateType) => {
+type RenderEntireThreeType = (state: StateType) => void;
+const renderEntireThree:RenderEntireThreeType = (state) => {
   ReactDOM.render(
     <App state={state} addPost={addPost}/>,
     document.getElementById('root')
