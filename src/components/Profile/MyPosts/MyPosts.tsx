16fc0263de24e89a7023d58ff34a827e3fabe119
jocker-py <buladzislau@gmail.com>
2023-03-17 14:18:33 +0300

    feat: add actions with textarea in MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {LegacyRef} from 'react';
 import Post, { PostType } from "./Post/Post";
 import styles from "./MyPosts.module.css";
 
@@ -7,18 +7,17 @@ const MyPosts = (props:MyPostsType) => {
   const postsElements = props.posts.map(
     ({id, message, likes}) =>
       <Post key={id} id={id} likes={likes} message={message}/>);
+  const newPostElement:LegacyRef<HTMLTextAreaElement> = React.createRef();
+  const showPost = () => {
+    alert(newPostElement.current!.value);
+    newPostElement.current!.value = '';
+  };
   return (
     <div className={styles.posts}>
       <h3>My posts</h3>
-      <div>
-        <textarea ></textarea>
-      </div>
-      <div>
-        <button>Add post</button>
-      </div>
-      <div className={styles.postList}>
-        {postsElements}
-      </div>
+      <div><textarea ref={newPostElement} /></div>
+      <div><button onClick={showPost}>Add post</button></div>
+      <div className={styles.postList}>{postsElements}</div>
   </div>
   );
 }
