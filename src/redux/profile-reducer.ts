ce1b4ed362b3f6c1ceb0a5ed82cb6e74ae14bf21
jocker-py <buladzislau@gmail.com>
2023-03-19 14:09:43 +0300

    feat: add profileReducer and type for it

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,8 +1,31 @@
 import {Types} from "../config/enums";
-import {IAction} from "./types";
+import {IAction, ProfilePageType} from "./types";
+import {PostType} from "../components/Profile/MyPosts/Post/Post";
 
 export type addPostActionCreatorType = () => IAction;
 export const addPostActionCreator:addPostActionCreatorType = () => ({type: Types.addPost});
 
 export type updatePostActionCreatorType = (text:string) => IAction
 export const updateNewPostActionCreator:updatePostActionCreatorType = (text) => ({type: Types.updateNewPost, text: text});
+
+
+type ProfileReducerType = (state: ProfilePageType, action: IAction) => ProfilePageType;
+const profileReducer: ProfileReducerType = (state, action) => {
+  switch (action.type) {
+    case Types.addPost:
+    const newPost: PostType = {
+      likes: 0,
+      message: state.newPostText,
+      id: state.posts.length + 1,
+    }
+      state.posts.push(newPost);
+      state.newPostText = '';
+      return state;
+    case Types.updateNewPost :
+    state.newPostText = action.text || '';
+    return state;
+    default : return state;
+  }
+}
+
+export default profileReducer;
