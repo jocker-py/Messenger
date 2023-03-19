ccaa66fe45425706422d0f323d3c179ad3190e46
jocker-py <buladzislau@gmail.com>
2023-03-19 14:09:55 +0300

    feat: add sidebarReducer and type for it

--- src/redux/sidebar-reducer.ts
+++ src/redux/sidebar-reducer.ts
@@ -1,1 +1,10 @@
-export {};
+import {IAction, SidebarType} from "./types";
+
+type SidebarReducerType = (state: SidebarType, action: IAction) => SidebarType;
+const sidebarReducer: SidebarReducerType = (state, action) => {
+  switch (action.type) {
+    default : return state;
+  }
+}
+
+export default sidebarReducer;
