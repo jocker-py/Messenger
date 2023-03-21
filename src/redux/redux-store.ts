75003988726894592aa4ac84591e1c4d3bb272e3
jocker-py <buladzislau@gmail.com>
2023-03-21 15:14:09 +0300

    feat: add combineReducers

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,7 +1,15 @@
-import {createStore} from "redux";
+import {combineReducers, createStore} from "redux";
+import sidebarReducer from "./sidebar-reducer";
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
 
+const reducers = () => combineReducers({
+  profilePage: profileReducer,
+  dialogsPage: dialogsReducer,
+  sidebar: sidebarReducer,
+})
 
-const store = createStore();
+const store = createStore(reducers);
 
 
 export default store;
