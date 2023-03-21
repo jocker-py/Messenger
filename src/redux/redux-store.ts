34859e18caece5571a085607681b291455f9b0a8
jocker-py <buladzislau@gmail.com>
2023-03-21 21:27:46 +0300

    fix: fix bug with reducers

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,13 +3,12 @@ import sidebarReducer from "./sidebar-reducer";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 
-const reducers = () => combineReducers({
+const reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
 })
 
 const store = createStore(reducers);
-
-
 export default store;
+
