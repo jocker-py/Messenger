b1508b39c1e510787178a14df641d12eb956e7dc
jocker-py <buladzislau@gmail.com>
2023-04-01 17:40:48 +0300

    refactor: remove store attribute in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
-import React, {FC} from 'react';
+import React, {FC} from "react";
 import {BrowserRouter, Route} from "react-router-dom";
-import './App.css';
-import Header from './components/Header/Header';
-import Profile from './components/Profile/Profile';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import Footer from "./components/Footer/Footer";
 import Sidebar from "./components/Sidebar/Sidebar";
 import {Path} from "./config/enums";
-import {StoreType} from "./redux/redux-store";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-
 
 interface IApp { store: StoreType }
 const App: FC<IApp> = ({store}) => {
-  const state = store.getState();
   return (
     <BrowserRouter>
-      <div className='app-wrapper'>
-        <Header />
-        <Sidebar state={state.sidebar}/>
-        <div className='app-wrapper-content'>
-          <Route path={Path.PROFILE} render={() => <Profile store={store}/>} />
-          <Route path={Path.DIALOGS} render={() => <DialogsContainer store={store}/>} />
-          <Route path={Path.NEWS} render={() => <News/>} />
-          <Route path={Path.MUSIC} render={() => <Music/>} />
-          <Route path={Path.SETTINGS} render={() => <Settings/>} />
+      <div className="app-wrapper">
+        <Header/>
+        <Sidebar/>
+        <div className="app-wrapper-content">
+          <Route path={Path.PROFILE} render={() => <Profile/>}/>
+          <Route path={Path.DIALOGS} render={() => <DialogsContainer/>}/>
+          <Route path={Path.NEWS} render={() => <News/>}/>
+          <Route path={Path.MUSIC} render={() => <Music/>}/>
+          <Route path={Path.SETTINGS} render={() => <Settings/>}/>
         </div>
-        <Footer />
+        <Footer/>
       </div>
     </BrowserRouter>
 
   );
-}
+};
 
 export default App;
