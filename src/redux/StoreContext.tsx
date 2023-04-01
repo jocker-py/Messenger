9a9d5c207abe8a4c8867f7bce5439155cfdc1cb5
jocker-py <buladzislau@gmail.com>
2023-04-01 17:37:16 +0300

    feat: add container Provider for StoreContext

--- src/redux/StoreContext.tsx
+++ src/redux/StoreContext.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, {FC} from "react";
 import store, {StoreType} from "./redux-store";
 
 export const StoreContext = React.createContext<StoreType>(store);
 
+export const Provider: FC = (props) => {
+  return <StoreContext.Provider value={store}>
+    {
+      props.children
+    }
+  </StoreContext.Provider>;
+};
