1ee43d82d32d56d020e2f584da00b464b38286dc
jocker-py <buladzislau@gmail.com>
2023-04-01 17:43:38 +0300

    feat: add context Consumer for DialogsContainer

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,25 +1,31 @@
-import React, {FC} from 'react';
+import React, {FC} from "react";
 import {sendMessageActionCreator, updateNewMessageTextActionCreator} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
-import {StoreType} from "../../redux/redux-store";
 import {DialogsType} from "../../redux/types";
+import {StoreContext} from "../../redux/StoreContext";
 
 type DialogsContainerType = { store: StoreType };
 const DialogsContainer: FC<DialogsContainerType> = (props) => {
-  const state = props.store.getState().dialogsPage as DialogsType;
-  const sendMessage = () => {
-    props.store.dispatch(sendMessageActionCreator());
-  }
-  const updateNewMessageText = (text: string) => {
-    props.store.dispatch(updateNewMessageTextActionCreator(text));
-  }
-  return (
-    <Dialogs dialogs={state.dialogs}
-             messages={state.messages}
-             newMessageText={state.newMessageText}
-             sendMessage={sendMessage}
-             updateNewMessageText={updateNewMessageText}/>
-  );
+  return <StoreContext.Consumer>
+    {
+      store => {
+        const state = store.getState().dialogsPage as DialogsType;
+        const sendMessage = () => {
+          store.dispatch(sendMessageActionCreator());
+        };
+        const updateNewMessageText = (text: string) => {
+          store.dispatch(updateNewMessageTextActionCreator(text));
+        };
+        return (
+          <Dialogs dialogs={state.dialogs}
+                   messages={state.messages}
+                   newMessageText={state.newMessageText}
+                   sendMessage={sendMessage}
+                   updateNewMessageText={updateNewMessageText}/>
+        );
+      }
+    }
+  </StoreContext.Consumer>;
 };
 
 export default DialogsContainer;
