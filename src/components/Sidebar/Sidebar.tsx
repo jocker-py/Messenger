8498de40514fd5b62d75014c593f6d5715ab04f1
jocker-py <buladzislau@gmail.com>
2023-04-01 17:42:32 +0300

    feat: add context Consumer for Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,26 @@
-import React, {FC} from 'react';
+import React, {FC} from "react";
 import Navbar from "./Navbar/Navbar";
 import styles from "./Sidbar.module.css";
 import Friends from "./Friends/Friends";
 import {SidebarType} from "../../redux/types";
+import {StoreContext} from "../../redux/StoreContext";
 
 type SidebarPropsType = { state: SidebarType };
 const Sidebar: FC<SidebarPropsType> = (props) => {
   return (
-    <div className={styles.sidebar}>
-      <Navbar state={props.state.navLinks}/>
-      <Friends state={props.state.friends}/>
-    </div>)
-}
+    <StoreContext.Consumer>
+      {store => {
+        const sidebar = store.getState().sidebar as SidebarType;
+        return (
+          <div className={styles.sidebar}>
+            <Navbar state={sidebar.navLinks}/>
+            <Friends state={sidebar.friends}/>
+          </div>);
+      }
+      }
+    </StoreContext.Consumer>
+  );
+};
 
 
 export default Sidebar;
