042e33695a9b17891499ddf74930a2048828a2a5
jocker-py <buladzislau@gmail.com>
2023-04-06 16:42:14 +0300

    refactor: make dialogsReduce pure function

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -4,7 +4,7 @@ import {DialogsType, IAction} from "./types";
 export type updateNewMessageTextActionCreatorType = (text: string) => IAction;
 export const updateNewMessageTextActionCreator: updateNewMessageTextActionCreatorType = (text: string) => ({
   type: ActionType.updateNewMessageText,
-  text: text
+  text: text,
 });
 
 export type sendMessageActionCreatorType = () => IAction;
@@ -12,35 +12,33 @@ export const sendMessageActionCreator: sendMessageActionCreatorType = () => ({ty
 
 const initialState: DialogsType = {
   dialogs: [
-    {id: 1, name: 'Dimych'},
-    {id: 2, name: 'Andrey'},
-    {id: 3, name: 'Sveta'},
-    {id: 4, name: 'Sasha'},
-    {id: 5, name: 'Viktor'},
-    {id: 6, name: 'Valera'},
+    {id: 1, name: "Dimych"},
+    {id: 2, name: "Andrey"},
+    {id: 3, name: "Sveta"},
+    {id: 4, name: "Sasha"},
+    {id: 5, name: "Viktor"},
+    {id: 6, name: "Valera"},
   ],
   messages: [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'How is your it-kamasutra?'},
-    {id: 3, message: 'Yo!'},
-    {id: 4, message: 'Yo!'},
-    {id: 5, message: 'Yo!'},
-    {id: 6, message: 'Yo!'},
+    {id: 1, message: "Hi"},
+    {id: 2, message: "How is your it-kamasutra?"},
+    {id: 3, message: "Yo!"},
+    {id: 4, message: "Yo!"},
+    {id: 5, message: "Yo!"},
+    {id: 6, message: "Yo!"},
   ],
-  newMessageText: '',
+  newMessageText: "",
 };
 
 type DialogsReducerType = (state: DialogsType, action: IAction) => DialogsType;
 const dialogsReducer: DialogsReducerType = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.sendMessage:
-      const newMessage = {message: state.newMessageText, id: state.messages.length + 1}
-      state.messages.push(newMessage);
-      state.newMessageText = '';
-      return state;
+      const newMessage = {message: state.newMessageText, id: state.messages.length + 1};
+      const oldMessages = state.messages.map(message => Object.assign({}, message));
+      return {...state, messages: [...oldMessages, newMessage], newMessageText: ""};
     case ActionType.updateNewMessageText:
-      state.newMessageText = action.text || '';
-      return state;
+      return {...state, newMessageText: action.text || ""};
     default :
       return state;
   }
