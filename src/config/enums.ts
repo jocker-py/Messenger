d51bf19ddf7f3f488ac5aa046a6fbaa64486410f
jocker-py <buladzislau@gmail.com>
2023-04-13 19:17:24 +0300

    feat: add setUsers and toggleFollow enum

--- src/config/enums.ts
+++ src/config/enums.ts
@@ -1,14 +1,17 @@
 export enum Path {
-  PROFILE = '/profile',
-  DIALOGS = '/dialogs',
-  MUSIC = '/music',
-  SETTINGS = '/settings',
-  NEWS = '/news',
+  PROFILE = "/profile",
+  DIALOGS = "/dialogs",
+  MUSIC = "/music",
+  SETTINGS = "/settings",
+  NEWS = "/news",
+  USERS = "/users",
 }
 
-export enum ActionType  {
-  addPost = 'ADD-POST',
-  updateNewPost = 'UPDATE-NEW-POST',
-  updateNewMessageText = 'UPDATE-NEW-MESSAGE-TEXT',
-  sendMessage = 'SEND-MESSAGE',
+export enum ActionType {
+  addPost = "ADD-POST",
+  updateNewPost = "UPDATE-NEW-POST",
+  updateNewMessageText = "UPDATE-NEW-MESSAGE-TEXT",
+  sendMessage = "SEND-MESSAGE",
+  setUsers = "SET-USERS",
+  toggleFollow = "TOGGLE-FOLLOW",
 }
