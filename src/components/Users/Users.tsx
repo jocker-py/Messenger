cd6bc1f351d08bb5134453520fd0888f701877bd
jocker-py <buladzislau@gmail.com>
2023-04-20 14:26:43 +0300

    refactor: change Users to functionComponent

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,38 +1,40 @@
-import React, {Component} from "react";
-import {UserType} from "../../redux/types";
-import axios from "axios";
+import React, {FC} from "react";
 import userPhoto from "../../assets/user.png";
+import s from "./Users.module.css";
+import {UsersContainerPropsType} from "./UsersContainer";
 
-type UsersPropsType = {
-  users: Array<UserType>;
-  setUsers: (users: Array<UserType>) => void;
-  toggleFollow: (id: number) => void;
+type UsersPropsType = UsersContainerPropsType & {
+  onPostChanged: (page: number) => void;
 }
 
-export class Users extends Component<UsersPropsType> {
-  componentDidMount() {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((res) => this.props.setUsers(res.data.items));
-  }
-
-  render() {
-    const usersElements = this.props.users.map((user) => (
-      <div key={user.id}>
-        <div>{user.name}</div>
-        <button onClick={() => this.props.toggleFollow(user.id)}>
-          {user.followed ? "follow" : "unfollow"}
-        </button>
-        <div>
-          <img src={user.photos.small || userPhoto} alt="userLogo"/>
-        </div>
-        <div>{user.uniqueUrlName}</div>
-        <div>{user.status}</div>
-      </div>));
-    return (
+const Users: FC<UsersPropsType> = (props) => {
+  const usersElements = props.users.map((user) => (
+    <div key={user.id} className={s.userItem}>
+      <div>{user.name}</div>
+      <button onClick={() => props.toggleFollow(user.id)}>
+        {user.followed ? "follow" : "unfollow"}
+      </button>
       <div>
-        {usersElements}
+        <img className={s.userImg} src={user.photos.small || userPhoto} alt="userLogo"/>
       </div>
-    );
-  }
+      <div>{user.uniqueUrlName}</div>
+      <div>{user.status}</div>
+    </div>));
+  const pages = Math.ceil(props.totalUsersCount / props.pageSize);
+  const pagesElements = pages && new Array(pages).slice(0, 5).fill(1)
+    .map((page, idx) => page + idx)
+    .map(page => <div key={page}
+                      onClick={() => props.onPostChanged(page)}
+                      className={page === props.currentPage ?
+                        `${s.pageItem} ${s.selectedPageItem}`
+                        : s.pageItem}>{page}</div>);
+  return (
+    <>
+      <div className={s.userList}>{usersElements}</div>
+      <div className={s.pageList}>{pages && pagesElements}</div>
+    </>
+  );
 };
+
+
+export default Users;
