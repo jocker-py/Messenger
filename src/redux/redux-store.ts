cf6f42e6daf0cfabaaf2e49100c80e34383b1b98
jocker-py <buladzislau@gmail.com>
2023-04-20 14:28:48 +0300

    feat: add users-reducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,14 +2,15 @@ import {combineReducers, createStore} from "redux";
 import sidebarReducer from "./sidebar-reducer";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
+import usersReducer from "./users-reducer";
 
 const reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
-})
+  usersPage: usersReducer,
+});
 
 const store = createStore(reducers);
-export type StoreType = typeof store;
 export default store;
 
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -8,23 +8,42 @@ export const setUsersAC: SetUsersACType = (users) => ({type: ActionType.setUsers
 type ToggleFollowACType = (id: number) => IAction;
 export const toggleFollowAC: ToggleFollowACType = (id) => ({type: ActionType.toggleFollow, id});
 
-const initialState = {
+type SetTotalUsersCountAC = (usersCount: number) => IAction;
+export const setTotalUsersCountAC: SetTotalUsersCountAC = (usersCount) => ({
+  type: ActionType.setTotalUsersCount,
+  usersCount,
+});
+
+type SetCurrentPageAC = (page: number) => IAction;
+export const setCurrentPageAC: SetCurrentPageAC = (page) => ({
+  type: ActionType.setCurrentPage,
+  page,
+});
+
+const initialState: UsersType = {
   users: [],
+  totalUsersCount: 0,
+  currentPage: 1,
+  pageSize: 10,
 };
 
 type UsersReducerType = (state: UsersType, action: IAction) => UsersType;
-const usersReducer: UsersReducerType = (state = initialState, action) => {
-  switch (action.type) {
+const usersReducer: UsersReducerType = (
+  state = initialState,
+  {id, type, users, usersCount, page},
+) => {
+  switch (type) {
     case ActionType.setUsers:
-      if (action.users && action.users.length) {
-        return {users: [...state.users, ...action.users]};
-      }
-      return state;
+      return users ? {...state, users: [...users]} : state;
     case ActionType.toggleFollow:
       return {
-        users: state.users.map(user => user.id === action.id ?
+        ...state, users: state.users.map(user => user.id === id ?
           {...user, followed: !user.followed} : user),
       };
+    case ActionType.setTotalUsersCount:
+      return usersCount ? {...state, totalUsersCount: usersCount} : state;
+    case ActionType.setCurrentPage:
+      return page ? {...state, currentPage: page} : state;
     default :
       return state;
   }
