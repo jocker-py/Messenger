de19a0cc5c2c359e69b5d6dc46bc5f436c0781ec
jocker-py <buladzislau@gmail.com>
2023-04-21 15:56:41 +0300

    feat: add styles for ProfileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,12 @@ import React, {FC} from "react";
 import styles from "./ProfileInfo.module.css";
 import {UserProfileType} from "../../../redux/types";
 import {Loader} from "../../common/Loader/Loader";
+import avatar from "../../../assets/user.png";
+import react from "../../../assets/react.png";
+import redux from "../../../assets/redux.png";
+import ts from "../../../assets/ts.png";
+import css from "../../../assets/css.png";
+import js from "../../../assets/js.png";
 
 type ProfileInfoType = {
   userProfile: UserProfileType
@@ -11,34 +17,40 @@ const ProfileInfo: FC<ProfileInfoType> = ({userProfile}) => {
   if (!userProfile) {
     return <Loader/>;
   }
-  const {userId, lookingForAJob, lookingForAJobDescription, fullName, contacts, photos} = userProfile;
-  const contactsElements = [];
-  for (let i in contacts) {
-    const contact = <div>{i}</div>;
-    contactsElements.push(contact);
-  }
-  const photosElements = [];
-  for (let i in photos) {
-    const photo = <div>{i}</div>;
-    photosElements.push(photo);
-  }
+  const icons: { [key: string]: string } = {react, redux, ts, js, css};
+  const {
+    userId, lookingForAJob, lookingForAJobDescription,
+    fullName, contacts, photos,
+  } = userProfile;
+  console.log(userProfile);
+  const wishlist = lookingForAJobDescription && lookingForAJobDescription.split(" ").map(item => {
+    item = item.trim().toLowerCase().replaceAll(/\W/g, "");
+    if (item in icons) {
+      return <li><img className={styles.wishImg} src={icons[item]} alt={item}/></li>;
+    }
+    return <li>{item}</li>;
+  });
   return (
-    <div>
-      <div>
+    <div className={styles.mainContainer}>
+      <div className={styles.imgContainer}>
         <img className={styles.img}
-             src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600"
-             alt="mountains"/>
+             src={photos.large || avatar}
+             alt={fullName || "avatar"}/>
       </div>
-      <div className={styles.description}>ava + description</div>
-      <div>{userId}</div>
-      <div>{lookingForAJob}</div>
-      <div>{lookingForAJobDescription}</div>
-      <div>{fullName}</div>
-      <div>
-        <div>{contactsElements}</div>
-        <div>{photosElements}</div>
+      <div className={styles.description}>
+        <h2>{fullName}</h2>
+        <div>{userId}</div>
+        <button>{lookingForAJob ? "I'm looking for a job" : "I'm happy working for company"}</button>
+        {
+          lookingForAJobDescription && (
+            <div className={styles.wishContainer}>
+              <h4 className={styles.wishTitle}>I would like to work with</h4>
+              <ul className={styles.wishList}>
+                {wishlist}
+              </ul>
+            </div>)
+        }
       </div>
-      <div></div>
     </div>);
 };
 
