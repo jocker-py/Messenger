1dc9e265568207609fa5fcac3430deb04bd5f9e9
jocker-py <buladzislau@gmail.com>
2023-04-21 12:10:54 +0300

    feat: add info for ProfileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,15 +1,45 @@
-import React, {FC} from 'react';
+import React, {FC} from "react";
 import styles from "./ProfileInfo.module.css";
+import {UserProfileType} from "../../../redux/types";
+import {Loader} from "../../common/Loader/Loader";
 
-const ProfileInfo: FC = () => {
-  return (<div>
-    <div>
-      <img className={styles.img}
-           src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600"
-           alt="mountains"/>
-    </div>
-    <div className={styles.description}>ava + description</div>
-  </div>)
+type ProfileInfoType = {
+  userProfile: UserProfileType
 }
+
+const ProfileInfo: FC<ProfileInfoType> = ({userProfile}) => {
+  if (!userProfile) {
+    return <Loader/>;
+  }
+  const {userId, lookingForAJob, lookingForAJobDescription, fullName, contacts, photos} = userProfile;
+  const contactsElements = [];
+  for (let i in contacts) {
+    const contact = <div>{i}</div>;
+    contactsElements.push(contact);
+  }
+  const photosElements = [];
+  for (let i in photos) {
+    const photo = <div>{i}</div>;
+    photosElements.push(photo);
+  }
+  return (
+    <div>
+      <div>
+        <img className={styles.img}
+             src="https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1600"
+             alt="mountains"/>
+      </div>
+      <div className={styles.description}>ava + description</div>
+      <div>{userId}</div>
+      <div>{lookingForAJob}</div>
+      <div>{lookingForAJobDescription}</div>
+      <div>{fullName}</div>
+      <div>
+        <div>{contactsElements}</div>
+        <div>{photosElements}</div>
+      </div>
+      <div></div>
+    </div>);
+};
 
 export default ProfileInfo;
