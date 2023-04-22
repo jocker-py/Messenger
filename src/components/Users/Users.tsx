49eb3ba68286cb1f0da4cc261de4d6ce1b490247
jocker-py <buladzislau@gmail.com>
2023-04-22 19:03:29 +0300

    feat: add request to server for follow and unfollow

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,9 +1,11 @@
-import React, {FC, MouseEvent} from "react";
+import React, {FC} from "react";
 import s from "./Users.module.css";
 import {UsersContainerPropsType} from "./UsersContainer";
 import {Loader} from "../common/Loader/Loader";
-import {Path} from "../../config/enums";
+import {EndPoint, Path} from "../../config/enums";
 import User from "./User";
+import axios from "axios";
+
 
 type UsersPropsType = UsersContainerPropsType & {
   onPostChanged: (page: number) => void;
@@ -12,15 +14,34 @@ type UsersPropsType = UsersContainerPropsType & {
 const Users: FC<UsersPropsType> = (props) => {
   const usersElements = props.users.map((user) => {
     const pathToUser = `${Path.PROFILE}/${user.id}`;
-    const toggleFollow = (e: MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      props.toggleFollow(user.id);
+    const follow = () => {
+      axios.post(EndPoint.follow + user.id, {
+        headers: {
+          "API-KEY": "094d9589-12fb-4e02-9b82-0b510c02fcd9",
+        },
+      }, {
+        withCredentials: true,
+      }).then((res) => {
+        if (res.data.resultCode === 0) {
+          props.follow(user.id);
+        }
+      });
+    };
+    const unfollow = () => {
+      axios.delete(EndPoint.follow + user.id, {
+        withCredentials: true,
+      }).then((res) => {
+        if (res.data.resultCode === 0) {
+          props.unfollow(user.id);
+        }
+      });
     };
     return <User key={user.id}
                  className={s.userItem}
                  user={user}
-                 toggleFollow={toggleFollow}
-                 path={pathToUser}/>
+                 follow={follow}
+                 unfollow={unfollow}
+                 path={pathToUser}/>;
   });
   const pages = Math.ceil(props.totalUsersCount / props.pageSize);
   const pagesElements = pages && new Array(pages).slice(0, 5).fill(1)
