e4857504d27e24311a6ad76699623c00dc8fb184
jocker-py <buladzislau@gmail.com>
2023-04-22 21:31:18 +0300

    refactor: use usersAPI instead of axios.post and axios.delete

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,9 +2,9 @@ import React, {FC} from "react";
 import s from "./Users.module.css";
 import {UsersContainerPropsType} from "./UsersContainer";
 import {Loader} from "../common/Loader/Loader";
-import {EndPoint, Path} from "../../config/enums";
+import {Path} from "../../config/enums";
 import User from "./User";
-import axios from "axios";
+import {usersAPI} from "../../api/api";
 
 
 type UsersPropsType = UsersContainerPropsType & {
@@ -12,36 +12,22 @@ type UsersPropsType = UsersContainerPropsType & {
 }
 
 const Users: FC<UsersPropsType> = (props) => {
-  const usersElements = props.users.map((user) => {
+  const usersElements = props.users.map(user => {
     const pathToUser = `${Path.PROFILE}/${user.id}`;
     const follow = () => {
-      axios.post(EndPoint.follow + user.id, {
-        headers: {
-          "API-KEY": "094d9589-12fb-4e02-9b82-0b510c02fcd9",
-        },
-      }, {
-        withCredentials: true,
-      }).then((res) => {
-        if (res.data.resultCode === 0) {
-          props.follow(user.id);
-        }
-      });
+      usersAPI
+        .follow(user.id)
+        .then(res => res && props.follow(user.id));
     };
     const unfollow = () => {
-      axios.delete(EndPoint.follow + user.id, {
-        withCredentials: true,
-      }).then((res) => {
-        if (res.data.resultCode === 0) {
-          props.unfollow(user.id);
-        }
-      });
+      usersAPI
+        .unfollow(user.id)
+        .then(res => res && props.unfollow(user.id));
     };
-    return <User key={user.id}
-                 className={s.userItem}
-                 user={user}
-                 follow={follow}
-                 unfollow={unfollow}
-                 path={pathToUser}/>;
+
+    return <User key={user.id} className={s.userItem} user={user}
+                 follow={follow} unfollow={unfollow} path={pathToUser}/>;
+
   });
   const pages = Math.ceil(props.totalUsersCount / props.pageSize);
   const pagesElements = pages && new Array(pages).slice(0, 5).fill(1)
