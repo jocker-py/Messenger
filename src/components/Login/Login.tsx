1ff55c7aa6c822b816bb36b8f76b567e8bffa317
jocker-py <buladzislau@gmail.com>
2023-05-06 15:31:31 +0300

    feat: add reduxForm for Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import LoginForm, {FormDataType} from "./LoginForm/LoginForm";
 
 const Login = () => {
+  const onSubmitForm = (formData: FormDataType) => {
+    console.log(formData);
+  };
   return (
-    <h1>
-      Login
-    </h1>
+    <div>
+      <h1>Login</h1>
+      <LoginForm onSubmit={onSubmitForm}/>
+    </div>
   );
 };
 
