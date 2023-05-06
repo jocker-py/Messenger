974428587dccf47e1cdf920d0e67b83b0b9e149f
jocker-py <buladzislau@gmail.com>
2023-05-06 15:44:26 +0300

    feat: use AddTextForm in MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,26 +1,25 @@
-import React, {ChangeEvent, FC} from "react";
+import React, {FC} from "react";
 import Post from "./Post/Post";
 import styles from "./MyPosts.module.css";
 import {ProfileType} from "../../../redux/types";
+import {AddTextForm, FormDataType} from "./AddTextForm/AddTextForm";
+
 
 type MyPostsType = ProfileType & {
-  addPost: () => void,
+  addPost: (text: string) => void
 };
 
 const MyPosts: FC<MyPostsType> = (props) => {
   const postsElements = props.posts.map(
     ({id, message, likes}) =>
       <Post key={id} id={id} likes={likes} message={message}/>);
-  const addPost = () => {
-    props.addPost();
+  const onAddPost = ({postMessage}: FormDataType) => {
+    props.addPost(postMessage);
   };
   return (
     <div className={styles.posts}>
       <h3>My posts</h3>
-      <div><textarea/></div>
-      <div>
-        <button onClick={addPost}>Add post</button>
-      </div>
+      <AddTextForm onSubmit={onAddPost}/>
       <div className={styles.postList}>{postsElements}</div>
     </div>
   );
