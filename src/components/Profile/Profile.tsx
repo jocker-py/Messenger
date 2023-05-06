1d3a2725ded1e49a2b1185ac97fcc2d28b6fee20
jocker-py <buladzislau@gmail.com>
2023-05-06 15:49:50 +0300

    feat: add ProfilePropsType

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,13 +4,18 @@ import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import {ProfileType} from "../../redux/types";
 import {Loader} from "../common/Loader/Loader";
 
-const Profile: FC<ProfileType> = (props) => {
-  if (!props.userProfile) {
+type ProfilePropsType = ProfileType & {
+  updateProfileStatus: (status: string) => void
+}
+
+const Profile: FC<ProfilePropsType> = ({profileStatus, updateProfileStatus, userProfile}) => {
+  if (!userProfile) {
     return <Loader/>;
   }
   return (
     <div>
-      <ProfileInfo userProfile={props.userProfile}/>
+      <ProfileInfo userProfile={userProfile} profileStatus={profileStatus}
+                   updateStatus={updateProfileStatus}/>
       <MyPostsContainer/>
     </div>
   );
