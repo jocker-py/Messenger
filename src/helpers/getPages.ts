65bb4b27d72c0f6585a7a6f4e926c17ed753f713
jocker-py <buladzislau@gmail.com>
2023-05-06 15:33:02 +0300

    refactor: change value of pages for view

--- src/helpers/getPages.ts
+++ src/helpers/getPages.ts
@@ -2,7 +2,7 @@ type GetPagesType = (totalCount: number, pageSize: number) => Array<number>;
 export const getPages: GetPagesType = (totalCount, pageSize) => {
   const pages = Math.ceil(totalCount / pageSize);
   return pages ?
-    new Array(pages).slice(0, 5).fill(1)
+    new Array(pages).slice(0, 20).fill(1)
       .map((page, idx) => page + idx) :
     [];
 };
