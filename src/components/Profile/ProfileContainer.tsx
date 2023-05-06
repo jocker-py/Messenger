5452b4cb62b63a963bc31a7875e8509f8859c132
jocker-py <buladzislau@gmail.com>
2023-05-06 15:49:10 +0300

    feat: add props for MapDispatchToProps and MapStateToProps

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import Profile from "./Profile";
-import {getProfile} from "../../redux/profile-reducer";
+import {getProfile, getProfileStatus, updateProfileStatus} from "../../redux/profile-reducer";
 import {ProfileType, StateType} from "../../redux/types";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
@@ -19,6 +19,7 @@ class ProfileContainer extends Component<ProfileContainerPropsType> {
   componentDidMount() {
     const userId = this.props.match.params.userId || 2;
     this.props.getProfile(userId);
+    this.props.getProfileStatus(userId);
   }
 
   render() {
@@ -30,16 +31,24 @@ type MapStateToPropsType = ProfileType;
 const mapStateToProps = (state: StateType): MapStateToPropsType => {
   return {
     posts: state.profilePage.posts,
-    newPostText: state.profilePage.newPostText,
     userProfile: state.profilePage.userProfile,
+    profileStatus: state.profilePage.profileStatus,
   };
 };
 
 type MapDispatchToPropsType = {
   getProfile: (userId: number) => void
+  getProfileStatus: (userId: number) => void
+  updateProfileStatus: (status: string) => void
 }
 
 
 export default compose<any>(
-  connect<MapStateToPropsType, MapDispatchToPropsType, {}, StateType>(mapStateToProps, {getProfile}),
-  withRouter, withAuthRedirect)(ProfileContainer);
+  connect<MapStateToPropsType, MapDispatchToPropsType, {}, StateType>(mapStateToProps, {
+    getProfile,
+    getProfileStatus,
+    updateProfileStatus,
+  }),
+  withRouter)(ProfileContainer);
+
+// TODO: withAuthRedirect
